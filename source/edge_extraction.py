68f7b8842d8fb1a73164483def43d3fb7a96df88
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-31 12:47:35 +0530

    Update edge_extraction.py

--- source/edge_extraction.py
+++ source/edge_extraction.py
@@ -24,3 +24,26 @@ print(edges_sobel_horizontal)
 
 print(edges_sobel_vertical)
 
+#plotting the sobel horizontal edges
+imshow(edges_sobel_horizontal,cmap='gray')
+
+plt.show()
+
+
+#plotting the sobel vertical edges
+imshow(edges_sobel_vertical,cmap='gray')
+
+plt.show()
+
+## Prewitt Kernel
+#calculating horizontal edges using prewitt kernel
+edges_prewitt_horizontal = prewitt_h(image)
+
+#calculating vertical edges using prewitt kernel
+edges_prewitt_vertical = prewitt_v(image)
+
+#plotting prewitt horizontal edges
+imshow(edges_prewitt_horizontal,cmap='gray')
+
+
+plt.show()
