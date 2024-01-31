10564f8076b39f1fee94272acd6a42715a020827
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-31 12:04:22 +0530

    sync

--- source/edge_extraction.py
+++ source/edge_extraction.py
@@ -3,3 +3,6 @@
 from skimage.io import imread, imshow
 from skimage.filters import sobel_h,prewitt_h,prewitt_v,sobel_v
 
+#reading the image and plotting it
+image = imread('/Users/paramanandbhat/Downloads/Extracting Edges/dog.jpg', as_gray=True)
+imshow(image,cmap='gray')
