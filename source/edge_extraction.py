663a21220c1820732d120492d0f1ab32ae1272a1
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-31 12:41:31 +0530

    image shape

--- source/edge_extraction.py
+++ source/edge_extraction.py
@@ -2,7 +2,14 @@
 #Import the required libraries
 from skimage.io import imread, imshow
 from skimage.filters import sobel_h,prewitt_h,prewitt_v,sobel_v
-
+import matplotlib.pyplot as plt
 #reading the image and plotting it
 image = imread('/Users/paramanandbhat/Downloads/Extracting Edges/dog.jpg', as_gray=True)
 imshow(image,cmap='gray')
+
+plt.show()
+
+print(image.shape)
+
+## Sobel Kernel
+
